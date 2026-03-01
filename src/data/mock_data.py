a92a176f32ba02795c8ce26efeddc226ffd9befe
efratMot <efrat4945@gmail.com>
2026-03-01 14:51:24 +0200

    Create mock products

---
+++ src/data/mock_data.py
@@ -0,0 +1,13 @@
+products = [
+    {"product_id": 1, "product_name": "violin", "product_category": "musical_instrumental", "price": 7000},
+    {"product_id": 2, "product_name": "guitar", "product_category": "musical_instrumental", "price": 3500},
+    {"product_id": 3, "product_name": "piano", "product_category": "musical_instrumental", "price": 12000},
+    {"product_id": 4, "product_name": "drum_set", "product_category": "musical_instrumental", "price": 4500},
+    {"product_id": 5, "product_name": "flute", "product_category": "musical_instrumental", "price": 2000},
+    {"product_id": 6, "product_name": "saxophone", "product_category": "musical_instrumental", "price": 8000},
+    {"product_id": 7, "product_name": "microphone", "product_category": "audio_equipment", "price": 900},
+    {"product_id": 8, "product_name": "headphones", "product_category": "audio_equipment", "price": 600},
+]
+
+customers = [{"customer_id": 101}, {"customer_id": 102}, {"customer_id": 103}, {"customer_id": 104},
+             {"customer_id": 105}]
