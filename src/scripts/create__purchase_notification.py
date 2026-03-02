e6d78107385a9a9b17b97adc7a80d379d6366de2
efratMot <efrat4945@gmail.com>
2026-03-02 16:42:24 +0200

    Using random.choices for Uneven distribution

--- src/scripts/create__purchase_notification.py
+++ src/scripts/create__purchase_notification.py
@@ -6,10 +6,10 @@ import random
 
 
 def create_purchase_notification():
-    product_index = random.randint(0, len(products) - 1)
+    weights = [p.get("popularity", 1) for p in products]
+    product = random.choices(products, weights=weights, k=1)[0]
     customer_index = random.randint(0, len(customers) - 1)
     quantity = random.randint(1, 10)
-    product = products[product_index]
     customer = customers[customer_index]
     new_purchase = PurchaseNotification(product_id=product["product_id"], product_name=product["product_name"],
                                         product_category=product["product_category"], price=product["price"],
