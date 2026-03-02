54a2345aa436e6b17001926f6fd075cc4ecf212a
efratMot <efrat4945@gmail.com>
2026-03-02 16:46:24 +0200

    Initial the purchase_publisher

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello efrat")
+import logging
+from src.scripts.purchase_publisher import PurchasePublisher
+import yaml
+
+
+def main():
+    config = yaml.safe_load(open("config.yml"))
+    logging.basicConfig(level=config["logging"]["level"], format=config["logging"]["format"])
+    logging.getLogger('pika').setLevel(logging.WARNING)
+    publisher = PurchasePublisher(
+        amqp_url=config['rabbitmq']['url'],
+        queue_name=config['rabbitmq']['queue'],
+        routing_key=config['rabbitmq']['routing_key'],
+        exchange=config['rabbitmq'].get('exchange', '')
+    )
+    publisher.run()
+
+
+if __name__ == '__main__':
+    main()
