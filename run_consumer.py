8e51d73f35d73f8a8d0e70d2e846697c638aba87
efratMot <efrat4945@gmail.com>
2026-03-05 21:05:50 +0200

    Adding create_kafka_producer method

--- run_consumer.py
+++ run_consumer.py
@@ -3,6 +3,7 @@ import yaml
 from src.base_reconnector import ReconnectingConsumer
 from src.consumers.purchase_consumer import PurchaseConsumer
 from src.models.redis_repository import RedisRepository
+from src.publishers.kafka_producer import KafkaRepository
 
 
 def main():
@@ -17,11 +18,16 @@ def main():
         redis_port = config["redis"]["port"]
         return RedisRepository(redis_host, redis_port)
 
+    def create_kafka_producer():
+        kafka_config = config["kafka"]
+        return KafkaRepository(kafka_config)
+
     consumer_params = {
         'amqp_url': amqp_url,
         'queue_name': queue_name,
         'exchange': 'purchases_exchange',
-        'redis_factory': create_redis
+        'redis_factory': create_redis,
+        'kafka_factory': create_kafka_producer
     }
     consumer = ReconnectingConsumer(PurchaseConsumer, **consumer_params)
     consumer.run()
