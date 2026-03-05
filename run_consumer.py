c1f19c1761abd95477a053f66c355e50e30acaad
efratMot <efrat4945@gmail.com>
2026-03-05 16:49:02 +0200

    Add create_redis function

--- run_consumer.py
+++ run_consumer.py
@@ -11,17 +11,21 @@ def main():
     logging.getLogger('pika').setLevel(logging.WARNING)
     amqp_url = config["rabbitmq"]["url"]
     queue_name = config["rabbitmq"]["queue"]
-    redis_host = config["redis"]["host"]
-    redis_port = config["redis"]["port"]
+
+    def create_redis():
+        redis_host = config["redis"]["host"]
+        redis_port = config["redis"]["port"]
+        return RedisRepository(redis_host, redis_port)
 
     consumer_params = {
         'amqp_url': amqp_url,
         'queue_name': queue_name,
         'exchange': 'purchases_exchange',
-        'redis_repository' : RedisRepository(redis_host, redis_port),
+        'redis_factory': create_redis
     }
     consumer = ReconnectingConsumer(PurchaseConsumer, **consumer_params)
     consumer.run()
+
 
 if __name__ == '__main__':
     main()
