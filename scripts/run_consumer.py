6ca6c806ed6045036996f249281f506cf8b14d13
efratMot <efrat4945@gmail.com>
2026-03-08 18:21:17 +0200

    Use PurchaseService instead of redis_factory /kafka_factory

--- scripts/run_consumer.py
+++ scripts/run_consumer.py
@@ -1,35 +1,37 @@
 import logging
 import yaml
-from src.base_reconnector import ReconnectingConsumer
-from src.consumers.purchase_consumer import PurchaseConsumer
-from src.models.redis_repository import RedisRepository
-from src.publishers.kafka_producer import KafkaRepository
+from pika.exchange_type import ExchangeType
+from src.services.purchase_service import PurchaseService
+from src.utils.consumer_reconnector import ConsumerReconnector
+from src.infrastructure.consumers.purchase_consumer import PurchaseConsumer
+from src.infrastructure.repositories.redis_repository import RedisRepository
+from src.infrastructure.producers.kafka_producer import KafkaProducer
 
 
 def main():
-    config = yaml.safe_load(open("config.yml"))
+    config = yaml.safe_load(open("../config/config.yml"))
     logging.basicConfig(level=config["logging"]["level"], format=config["logging"]["format"])
     logging.getLogger('pika').setLevel(logging.WARNING)
     amqp_url = config["rabbitmq"]["url"]
     queue_name = config["rabbitmq"]["queue"]
 
-    def create_redis():
-        redis_host = config["redis"]["host"]
-        redis_port = config["redis"]["port"]
-        return RedisRepository(redis_host, redis_port)
+    redis_host = config["redis"]["host"]
+    redis_port = config["redis"]["port"]
+    redis= RedisRepository(redis_host, redis_port)
 
-    def create_kafka_producer():
-        kafka_config = config["kafka"]
-        return KafkaRepository(kafka_config)
+    kafka_config = config["kafka"]
+    kafka = KafkaProducer(kafka_config)
 
     consumer_params = {
         'amqp_url': amqp_url,
         'queue_name': queue_name,
         'exchange': 'purchases_exchange',
-        'redis_factory': create_redis,
-        'kafka_factory': create_kafka_producer
+        'routing_key': queue_name,
+        'exchange_type': ExchangeType.direct,
+        'data_service': PurchaseService(redis, kafka)
+
     }
-    consumer = ReconnectingConsumer(PurchaseConsumer, **consumer_params)
+    consumer = ConsumerReconnector(PurchaseConsumer, **consumer_params)
     consumer.run()
 
 
