042bf4fb56465770669c03c52b5338c1cdb9f3db
efratMot <efrat4945@gmail.com>
2026-03-08 18:20:04 +0200

    Adding close_connections method for safely shutdown

--- src/services/purchase_service.py
+++ src/services/purchase_service.py
@@ -6,6 +6,12 @@ class PurchaseService:
     def process(self, notification_dict, raw_body):
         success = self.redis.add_notification(notification_dict)
         if success:
-            self.kafka.publish_notification('purchase_topic', notification_dict['order_id'], raw_body)
-            return True
+            success = self.kafka.publish_notification('purchase_topic', notification_dict['order_id'], raw_body)
+            if success:
+                return True
+            return False
         return False
+
+    def close_connections(self):
+        self.redis.close_connection()
+        self.kafka.close_connection()
