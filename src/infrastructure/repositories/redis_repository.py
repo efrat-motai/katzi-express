9154638915aa7ac0a3f62a95b19cc78456a4d377
efratMot <efrat4945@gmail.com>
2026-03-15 18:09:20 +0200

    Change the method 'add_notification' to 'increment_product_count'

--- src/infrastructure/repositories/redis_repository.py
+++ src/infrastructure/repositories/redis_repository.py
@@ -1,34 +1,40 @@
-import json
 import logging
+import time
+
 import redis
 
 LOGGER = logging.getLogger(__name__)
 
+
 class RedisRepository:
-    def __init__(self, host="localhost", port=6379, ttl=0):
+    def __init__(self, host="localhost", port=6379, ttl=0, db=0):
         self.r = None
-        self.connect(host, port)
+        self.connect(host, port, db)
         self.ttl = ttl
 
-    def connect(self, host:str, port:int)->None:
+    def connect(self, host: str, port: int, db: int) -> None:
         try:
-            self.r = redis.Redis(host=host, port=port, decode_responses=True)
+            self.r = redis.Redis(host=host, port=port, db=db, decode_responses=True)
             self.r.ping()
             LOGGER.info("Successfully connected to Redis")
         except redis.ConnectionError as e:
             LOGGER.error(f"Redis connection error: {e}")
 
-    def add_notification(self, notification: dict) -> bool:
+    def increment_product_count(self, product_id: int) -> bool:
         try:
-            payload = json.dumps(notification)
-            success= self.r.setex(f"order:{notification['order_id']}", self.ttl, payload)
-            LOGGER.info(f"Successfully added to redis")
+            window_timestamp = int(time.time() // 60) * 60
+            key = f"hot_products:{window_timestamp}"
+            success = self.r.zincrby(key, 1, product_id)
+            self.r.expire(key, self.ttl)
+            LOGGER.info(f"Incremented count for product {product_id}")
             return success
+
         except redis.RedisError as e:
             LOGGER.error(f"Redis error: {e}")
             return False
 
-    def close_connection(self)->None:
+    
+    def close_connection(self) -> None:
         if self.r:
             self.r.close()
             LOGGER.info("Redis connection closed safely.")
