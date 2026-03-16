4ec3199d68fe4c13bd0fa06c0eefc11ca7a0efed
efratMot <efrat4945@gmail.com>
2026-03-16 02:16:41 +0200

    feat: delete unneccesary redis usages

--- src/services/purchase_service.py
+++ src/services/purchase_service.py
@@ -1,17 +1,12 @@
 class PurchaseService:
-    def __init__(self, redis_repo, kafka_producer):
-        self.redis = redis_repo
+    def __init__(self, kafka_producer):
         self.kafka = kafka_producer
 
     def process(self, notification:dict, raw_body)->bool:
-        success = self.redis.add_notification(notification)
+        success = self.kafka.publish_notification('purchase_topic', notification['order_id'], raw_body)
         if success:
-            success = self.kafka.publish_notification('purchase_topic', notification['order_id'], raw_body)
-            if success:
-                return True
-            return False
+            return True
         return False
 
     def close_connections(self)->None:
-        self.redis.close_connection()
         self.kafka.close_connection()
