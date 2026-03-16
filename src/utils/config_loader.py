da0dc2f8bf0586c7d881018207d380313eecfb89
efratMot <efrat4945@gmail.com>
2026-03-16 04:33:05 +0200

    feat: using dynamic path

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,11 +1,18 @@
 import logging
+import os
 
 import yaml
 
 
-def load_config(config_path="../../config/config.yml"):
+def load_config():
+    current_file_path = os.path.abspath(__file__)
+    utils_dir = os.path.dirname(current_file_path)
+    config_path = os.path.join(utils_dir, "..", "..", "config", "config.yml")
+    config_path = os.path.normpath(config_path)
     config = yaml.safe_load(open(config_path))
     logging.basicConfig(level=config["logging"]["level"], format=config["logging"]["format"])
     logging.getLogger('pika').setLevel(logging.WARNING)
     logging.getLogger('kafka').setLevel(logging.WARNING)
     return config
+
+
