02c1c4c4f4d3deb74ba09449acad00d4c9086956
efratMot <efrat4945@gmail.com>
2026-03-17 16:10:03 +0200

    refactor: inject product_repository

--- src/services/hot_product_service.py
+++ src/services/hot_product_service.py
@@ -1,16 +1,18 @@
 import json
 import logging
 import time
-
-from src.utils.data.mock_data import products
+from src.infrastructure.repositories.product.base_product_repository import BaseProductRepository
+from src.infrastructure.repositories.purchase.base_purchase_repository import BasePurchaseRepository
 
 LOGGER = logging.getLogger(__name__)
 
 
 class HotProductService:
 
-    def __init__(self, redis_repo, window_duration_minutes: int = 1):
-        self.redis = redis_repo
+    def __init__(self, redis_repo: BasePurchaseRepository, product_repo: BaseProductRepository,
+                 window_duration_minutes: int = 1):
+        self.redis_repository = redis_repo
+        self.product_repository = product_repo
         self.window_size_seconds = window_duration_minutes * 60
 
     def process(self, purchase_notification: str) -> bool:
@@ -23,7 +25,7 @@ class HotProductService:
             window_timestamp = int(
                 time.time() // self.window_size_seconds) * self.window_size_seconds
             key = f"hot_products:{window_timestamp}"
-            return self.redis.increment_product_count(key, product_id)
+            return self.redis_repository.increment_product_count(key, product_id)
         except json.JSONDecodeError:
             LOGGER.error("Invalid JSON received")
             return True
@@ -31,17 +33,18 @@ class HotProductService:
     def get_top_products(self, count) -> list:
         window_timestamp = int(time.time() // self.window_size_seconds) * self.window_size_seconds
         key = f"hot_products:{window_timestamp}"
-        row_result = self.redis.get_hot_products(key=key, count=count)
+        row_result = self.redis_repository.get_hot_products(key=key, count=count)
 
         if not row_result:
             key = f"hot_products:{window_timestamp - self.window_size_seconds // 60}"
-            row_result = self.redis.get_hot_products(key=key, count=3)
+            row_result = self.redis_repository.get_hot_products(key=key, count=3)
 
-        products_mock = {str(p["product_id"]): p for p in products}
+        products = self.product_repository.get_all()
+        products_by_id = {str(p["product_id"]): p for p in products}
         full_details = []
 
         for product_id, score in row_result:
-            product_info = products_mock.get(product_id)
+            product_info = products_by_id.get(product_id)
 
             if product_info:
                 result = product_info.copy()
@@ -51,4 +54,4 @@ class HotProductService:
         return full_details
 
     def close_connections(self) -> None:
-        self.redis.close_connection()
+        self.redis_repository.close_connection()
