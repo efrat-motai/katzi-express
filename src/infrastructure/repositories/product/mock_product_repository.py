c72d0aeaa19ae8f1b7cfff6435d25a687d2cd20f
efratMot <efrat4945@gmail.com>
2026-03-17 17:11:02 +0200

    feat: inplement get_by_id method

--- src/infrastructure/repositories/product/mock_product_repository.py
+++ src/infrastructure/repositories/product/mock_product_repository.py
@@ -5,3 +5,7 @@ from src.utils.data.mock_data import PRODUCTS
 class MockProductRepository(BaseProductRepository):
     def get_all(self) -> list:
         return PRODUCTS
+
+    def get_by_id(self, product_id: str) -> dict:
+        product = [p for p in PRODUCTS if str(p['product_id']) == product_id]
+        return product[0] if product else None
