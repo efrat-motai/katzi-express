46ba7d11759402fb919d7a9f2067d3f7b752fb53
efratMot <efrat4945@gmail.com>
2026-03-17 17:10:26 +0200

    feat: add get_by_id abstract method

--- src/infrastructure/repositories/product/base_product_repository.py
+++ src/infrastructure/repositories/product/base_product_repository.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 class BaseProductRepository(ABC):
     @abstractmethod
-    def get_all(self)->list:
+    def get_all(self) -> list:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, product_id: int) -> dict:
         pass
