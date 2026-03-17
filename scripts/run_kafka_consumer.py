b14db3dd47b32500cd08e1f383420ea790c379b0
efratMot <efrat4945@gmail.com>
2026-03-17 20:19:48 +0200

    refactor: use bootstrap_service for hot_product_service instance

--- scripts/run_kafka_consumer.py
+++ scripts/run_kafka_consumer.py
@@ -1,16 +1,11 @@
 from src.infrastructure.messaging.consumers.kafka_consumer import KafkaConsumer
-from src.infrastructure.repositories.product.mock_product_repository import MockProductRepository
-from src.infrastructure.repositories.purchase.redis_purchase_repository import RedisPurchaseRepository
-from src.services.hot_product_service import HotProductService
+from src.setup import bootstrap_service
 from src.utils.config_loader import load_config
 
 
 def main():
     config = load_config()
-    redis_config = config["redis"]
-    redis = RedisPurchaseRepository(host=redis_config["host"], port=redis_config["port"], ttl=redis_config["ttl"])
-
-    hot_product_service = HotProductService(redis,MockProductRepository() )
+    hot_product_service = bootstrap_service()
 
     kafka_config = config["kafka_consumer"]
     consumer = KafkaConsumer(hot_product_service)
--- src/api/hot_products_api.py
+++ src/api/hot_products_api.py
@@ -1,20 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
-
-from src.infrastructure.repositories.product.mock_product_repository import MockProductRepository
-from src.infrastructure.repositories.purchase.redis_purchase_repository import RedisPurchaseRepository
-from src.services.hot_product_service import HotProductService
-from src.utils.config_loader import load_config
+from src.setup import bootstrap_service
 
 app = FastAPI()
 
 @app.get("/hot_products")
-async def get_hot_products(count:int = 3):
-   return hot_product_service.get_top_products(count=count)
+async def get_hot_products(count: int = 3):
+    return hot_product_service.get_top_products(count=count)
+
 
 if __name__ == "__main__":
-    config = load_config()
-    redis_conf = config["redis"]
-    redis = RedisPurchaseRepository(redis_conf["host"], redis_conf["port"], redis_conf["ttl"])
-    hot_product_service = HotProductService(redis, MockProductRepository())
+    hot_product_service = bootstrap_service()
     uvicorn.run(app, host="0.0.0.0", port=8000)
