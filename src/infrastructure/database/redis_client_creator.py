df0eb088061ce259874fb24aad2101e8176c9011
efratMot <efrat4945@gmail.com>
2026-03-18 03:35:02 +0200

    fix: increase the tries value

--- src/infrastructure/database/redis_client_creator.py
+++ src/infrastructure/database/redis_client_creator.py
@@ -6,7 +6,7 @@ from retry import retry
 LOGGER = logging.getLogger(__name__)
 
 
-@retry(tries=5, delay=2, backoff=2, exceptions=redis.ConnectionError)
+@retry(tries=10, delay=2, backoff=2, exceptions=redis.ConnectionError)
 def create_redis_client(host="localhost", port=6379, db=0):
         client = redis.Redis(host=host, port=port, db=db, decode_responses=True)
         client.ping()
