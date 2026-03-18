5cbf6312fa58d8d75027eba68bac52e30e7ec0e5
efratMot <efrat4945@gmail.com>
2026-03-18 11:41:08 +0200

    refactor: use FastAPI Dependency Injection for HotProductService

--- src/api/hot_products_api.py
+++ src/api/hot_products_api.py
@@ -1,14 +1,14 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from src.setup import bootstrap_service
 
 app = FastAPI()
 
+
 @app.get("/hot_products")
-async def get_hot_products(count: int = 3):
-    return hot_product_service.get_top_products(count=count)
+async def get_hot_products(count: int = 3, service =Depends(bootstrap_service)):
+    return service.get_top_products(count=count)
 
 
 if __name__ == "__main__":
-    hot_product_service = bootstrap_service()
     uvicorn.run(app, host="0.0.0.0", port=8000)
