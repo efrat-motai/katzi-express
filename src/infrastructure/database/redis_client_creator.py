f44a5f3915568fa131163069ca0ac112510fac86
efratMot <efrat4945@gmail.com>
2026-03-22 06:05:19 +0200

    refactor: use redis retry mechanism

--- src/infrastructure/database/redis_client_creator.py
+++ src/infrastructure/database/redis_client_creator.py
@@ -1,14 +1,14 @@
 import redis
 import logging
-
-from retry import retry
+from redis.backoff import ExponentialBackoff
+from redis.retry import Retry
 
 LOGGER = logging.getLogger(__name__)
 
 
-@retry(tries=10, delay=2, backoff=2, exceptions=redis.ConnectionError)
 def create_redis_client(host="localhost", port=6379, db=0):
-        client = redis.Redis(host=host, port=port, db=db, decode_responses=True)
-        client.ping()
-        LOGGER.info(f"Successfully connected to Redis at {host}:{port}")
-        return client
+    retry = Retry(ExponentialBackoff(), 10)
+    client = redis.Redis(retry=retry, host=host, port=port, db=db, decode_responses=True)
+    client.ping()
+    LOGGER.info(f"Successfully connected to Redis at {host}:{port}")
+    return client
