0254a3415aaed862971c33a996525d055557abf0
efratMot <efrat4945@gmail.com>
2026-03-23 01:13:31 +0200

    fix: switch the key for the partition from order_id to product_id

--- src/services/purchase_service.py
+++ src/services/purchase_service.py
@@ -2,11 +2,11 @@ class PurchaseService:
     def __init__(self, kafka_producer):
         self.kafka = kafka_producer
 
-    def process(self, notification:dict, raw_body)->bool:
-        success = self.kafka.publish_notification('purchase_topic', notification['order_id'], raw_body)
+    def process(self, notification: dict, raw_body) -> bool:
+        success = self.kafka.publish_notification('purchase_topic', notification['product_id'], raw_body)
         if success:
             return True
         return False
 
-    def close_connections(self)->None:
+    def close_connections(self) -> None:
         self.kafka.close_connection()
