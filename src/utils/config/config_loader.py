6fe0b01e9b73246f523d1be6963c65e3937d252a
efratMot <efrat4945@gmail.com>
2026-03-25 14:10:44 +0200

    refactor: switch to use pathlib package instead of os

--- src/utils/config/config_loader.py
+++ src/utils/config/config_loader.py
@@ -1,18 +1,12 @@
 import logging
-import os
-
+from pathlib import Path
 import yaml
 
 
 def load_config():
-    current_file_path = os.path.abspath(__file__)
-    utils_dir = os.path.dirname(current_file_path)
-    config_path = os.path.join(utils_dir, "..", "..", "config", "config.yml")
-    config_path = os.path.normpath(config_path)
+    config_path = Path(__file__).resolve().parents[3]/"config"/"config.yml"
     config = yaml.safe_load(open(config_path))
     logging.basicConfig(level=config["logging"]["level"], format=config["logging"]["format"])
     logging.getLogger('pika').setLevel(logging.CRITICAL)
     logging.getLogger('kafka').setLevel(logging.WARNING)
     return config
-
-
