d33b8b3571845506a80fab81fd9b24d9f194b750
efratMot <efrat4945@gmail.com>
2026-03-25 14:11:19 +0200

    feat: adding profile_data field to redis_client

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,15 @@
 import logging
-
 from src.infrastructure.database.redis_client_creator import create_redis_client
 from src.infrastructure.repositories.product.mock_product_repository import MockProductRepository
 from src.infrastructure.repositories.purchase.redis_purchase_repository import RedisPurchaseRepository
 from src.services.hot_product_service import HotProductService
-from src.utils.config_loader import load_config
+from src.utils.config.config_loader import load_config
+from src.utils.types.redis_profile import RedisProfile
 
 LOGGER = logging.getLogger(__name__)
 
 
-def bootstrap_service() -> HotProductService | None:
+def bootstrap_service(profile:RedisProfile = RedisProfile.API) -> HotProductService | None:
     config = load_config()
     redis_config = config["redis"]
 
@@ -17,7 +17,8 @@ def bootstrap_service() -> HotProductService | None:
         redis_client = create_redis_client(
             host=redis_config["host"],
             port=redis_config["port"],
-            db=0
+            db=0,
+            profile_data=redis_config["profiles"][profile.value]
         )
     except Exception as e:
         LOGGER.critical(f"Could not connect to Redis. Application exiting. Error: {e}")
