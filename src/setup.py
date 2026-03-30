456414d5f8f5a8e4648f5c53460a0a7d52515505
efratMot <efrat4945@gmail.com>
2026-03-31 02:36:26 +0300

    feat: inject sliding implementation

--- src/setup.py
+++ src/setup.py
@@ -1,21 +1,21 @@
 import logging
-from src.infrastructure.database.redis_client_creator import create_redis_client
-from src.infrastructure.repositories.product.mock_product_repository import MockProductRepository
-from src.infrastructure.repositories.purchase.redis_purchase_repository import RedisPurchaseRepository
-from src.services.hot_product_service import HotProductService
+from src.infrastructure.storage.redis_client import get_redis_client
+from src.infrastructure.storage.product.mock_product_repository import MockProductRepository
+from src.infrastructure.storage.hot_products.hot_products_sliding_store import HotProductsSlidingStore
+from src.services.hot_products_sliding_service import HotProductsSlidingService
 from src.utils.config.config_loader import load_config
 from src.utils.types.redis_profile import RedisProfile
 
 LOGGER = logging.getLogger(__name__)
 
 
-def bootstrap_service(profile:RedisProfile = RedisProfile.API) -> HotProductService | None:
+def bootstrap_service(profile:RedisProfile = RedisProfile.API):
     config = load_config()
     redis_config = config["redis"]
 
     try:
         LOGGER.info(f"Attempting to connect to Redis (Profile: {profile.name})...")
-        redis_client = create_redis_client(
+        redis_client = get_redis_client(
             host=redis_config["host"],
             port=redis_config["port"],
             db=0,
@@ -27,6 +27,6 @@ def bootstrap_service(profile:RedisProfile = RedisProfile.API) -> HotProductServ
         LOGGER.critical(f"Could not connect to Redis. Application exiting. Error: {e}")
         return None
 
-    redis = RedisPurchaseRepository(ttl=redis_config["ttl"], redis_client=redis_client)
-    hot_product_service = HotProductService(redis, MockProductRepository())
+    redis = HotProductsSlidingStore(ttl=redis_config["ttl"], redis_client=redis_client)
+    hot_product_service = HotProductsSlidingService(redis, MockProductRepository())
     return hot_product_service
